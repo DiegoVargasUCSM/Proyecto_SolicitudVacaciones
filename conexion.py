6069bc443b8016057dc44f1d067657f83301bb82
DiegoVargasUCSM <diego.vargasv@ucsm.edu.pe>
2025-11-04 22:11:52 -0500

    Primera prueba d conexion de la base de datos

---
+++ conexion.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+def conectar():
+    try:
+        conexion = mysql.connector.connect(
+            host="localhost",
+            user="root",           
+            password="tu_contraseña",  
+            database="Solicitud_Vacaciones"
+        )
+        if conexion.is_connected():
+            print("Conexión exitosa a la base de datos Solicitud_Vacaciones...")
+            return conexion
+    except mysql.connector.Error as error:
+        print("Error al conectar a la base de datos:", error)
+        return None
+
+if __name__ == "__main__":
+    conectar()
